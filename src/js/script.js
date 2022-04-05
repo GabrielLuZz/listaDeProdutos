01cffad2d3fa02c70ac0db96a04e980032d6b15f
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-04-05 09:24:01 -0300

    layout mudado para o propósito da atividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
+filtrarPorTodos();
 
+
+botaoTodos.addEventListener('click', filtrarPorTodos)
+
+botaoHortifruit.addEventListener('click', filtrarPorHortifruit)
+
+botaoPanificadora.addEventListener('click', filtrarPorPanificadora)
+
+botaoLaticinios.addEventListener('click', filtrarPorLaticinios)
+
+botaoPesquisar.addEventListener('click', PesquisarProduto);
+
+input.addEventListener('keyup', (e) => {
+    if (input.value.length > 0) {
+        apagador.style.display = 'block';
+        apagador.addEventListener('click', () => {
+            input.value = '';
+            apagador.style.display = 'none';
+        })
+    } else {
+        apagador.style.display = 'none';
+    }
+
+    if (e.key === 'Enter') {
+        PesquisarProduto(e);
+    }
+})
